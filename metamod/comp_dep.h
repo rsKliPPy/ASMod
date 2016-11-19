0a36e25c6cb06603097d7d59164ed76c1efb944e
Sam <duo929@hotmail.com>
2016-11-19 17:37:35 +0100

    Update comp_dep.h to support newer compilers that provide va_copy

--- metamod/comp_dep.h
+++ metamod/comp_dep.h
@@ -74,14 +74,17 @@
 	#endif //defined WIN32
 #endif
 
-#if defined (_WIN32) && defined (_MSC_VER)
-	// On x86 va_list is just a pointer.
-	#define va_copy(dst,src) ((dst)=(src))
-#else
-	// Some systems that do not supply va_copy have __va_copy instead, since 
-	// that was the name used in the draft proposal.
-	#if !defined(__GNUC__) || __GNUC__ < 3
-		#define va_copy __va_copy
+//Only if it's not defined already. - Solokiller
+#if !defined( va_copy )
+	#if defined (_WIN32) && defined (_MSC_VER)
+		// On x86 va_list is just a pointer.
+		#define va_copy(dst,src) ((dst)=(src))
+	#else
+		// Some systems that do not supply va_copy have __va_copy instead, since 
+		// that was the name used in the draft proposal.
+		#if !defined(__GNUC__) || __GNUC__ < 3
+			#define va_copy __va_copy
+		#endif
 	#endif
 #endif
 
