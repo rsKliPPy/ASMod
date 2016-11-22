cbb5b3b4205fb30c961717403a16fd66b3c2ed57
Sam <duo929@hotmail.com>
2016-11-22 21:41:34 +0100

    Disable min & max macros, statically link with libstdc++ on Linux

--- hlsdk/dlls/extdll.h
+++ hlsdk/dlls/extdll.h
@@ -44,13 +44,13 @@ typedef int BOOL;
 #define MAX_PATH PATH_MAX
 #include <limits.h>
 #include <stdarg.h>
-#ifndef min
-#define min(a,b)  (((a) < (b)) ? (a) : (b))
-#endif
-#ifndef max
-#define max(a,b)  (((a) > (b)) ? (a) : (b))
-#define _vsnprintf(a,b,c,d) vsnprintf(a,b,c,d)
-#endif
+//#ifndef min
+//#define min(a,b)  (((a) < (b)) ? (a) : (b))
+//#endif
+//#ifndef max
+//#define max(a,b)  (((a) > (b)) ? (a) : (b))
+//#define _vsnprintf(a,b,c,d) vsnprintf(a,b,c,d)
+//#endif
 #endif //_WIN32
 
 // Misc C-runtime library headers
